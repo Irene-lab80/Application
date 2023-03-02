4e443266742305dfbce83f73313518d956c7b6fc
Irina <irene7744@gmail.com>
2023-03-02 13:48:29 +0300

    feat: add notFoundPage

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
-import styles from './NotFoundPage.module.scss';
+import catImage from '../../shared/assets/images/page-not-found.png';
+import style from './NotFoundPage.module.scss';
 
-export const NotFoundPage = () => <h2 className={styles.title}>Страница не найдена!</h2>;
+export const NotFoundPage = () => (
+  <div className="page-wrapper">
+    <div className={style.page__wrapper}>
+      <div className={style.page__info}>
+        <p className={style.page__message_bold}>Упс! Кажется, на эту страницу прилег котик</p>
+        <p className={style.page__message_error}>Ошибка 404</p>
+        <p className={style.page__message}>
+          Мы уже разбираемся, почему так получилось, скоро все починим.
+        </p>
+      </div>
+      <img className={style.page__img} src={catImage} alt="Page not found cat" />
+    </div>
+  </div>
+);
